f4fc58a25c0c9872b2c0e4470cf6ad4962cbc3af
gurukun41 <kouga1582@gmail.com>
2025-05-17 23:58:05 +0900

    404

---
+++ 400~/400~/ 404/d.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;                                  //long long型の一次元
+using vvl = vector<vl>;                                 //long long型の二次元配列
+using vvvl = vector<vvl>;                               //long long型の三次元配列
+using vi = vector<int>;                                 //int型の一次元
+using vvi = vector<vi>;                                 //int型の二次元配列
+using vvvi = vector<vvi>;                               //int型の三次元配列
+#define rep(i,a,b) for(int i = (a); i < (int)(b); i++)  //for文の短縮
+#define all(v) v.begin(), v.end()                       //all(v)でvの始まりと終わりのイテレーター
+//入力を受け取る
+template <typename T> 
+T input(){
+    T x;
+    cin >> x;
+    return x;
+}
+//a,bのうち最大のものをaに入れる(aがbに置き換わるときはtrueを返す)
+template <typename T>
+inline bool chmax(T &a, const T& b){
+    if(a < b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+//a,bのうち最小のものをaに入れる(aがbに置き換わるときはtrueを返す)
+template <typename T>
+inline bool chmin(T &a, const T& b){
+    if(a > b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+// 無限大の値
+const long long INF = 1LL << 60;
+int main(){
+
+}
