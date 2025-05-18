ad9a243417298941635c57bc434232f37e108750
gurukun41 <kouga1582@gmail.com>
2025-05-18 16:01:35 +0900

    アルゴ基礎第三回アルゴリズム

---
+++ Algorithm_Basics/Algorithm/CumulativeSum .cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<ll> A(10000);
+vector<ll> S(10000);
+int N;
+
+void make_table(){ //配列を作る
+    for (int i = 0; i < N; i++){
+        S[i + 1] = S[i] + A[i];
+    }
+}
+
+void calc_sum(int l, int r){ // 連続した要素の和を計算, [l, r], 0-indexed
+    cout << S[r + 1] - S[l] << endl;
+}
+
+int main(){
+    cin >> N;for(int i = 0; i < N; i ++){cin >> A[i];}
+    make_table();
+    int l, r;cin >> l >> r;
+    calc_sum(l,r);
+}
