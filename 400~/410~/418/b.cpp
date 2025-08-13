36c8ce9d8102d3042f5aadb0ce0dd0068e2238ea
gurukun41 <kouga1582@gmail.com>
2025-08-13 18:50:52 +0900

    418

---
+++ 400~/410~/418/b.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+using vl = vector<ll>;                                     // long long型の一次元
+using vvl = vector<vl>;                                    // long long型の二次元配列
+using vvvl = vector<vvl>;                                  // long long型の三次元配列
+using vi = vector<int>;                                    // int型の一次元
+using vvi = vector<vi>;                                    // int型の二次元配列
+using vvvi = vector<vvi>;                                  // int型の三次元配列
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)  // for文の短縮
+#define all(v) v.begin(), v.end()                          // all(v)でvの始まりと終わりのイテレーター
+
+// 入力を受け取る
+template <typename T>
+T input() {
+    T x;
+    cin >> x;
+    return x;
+}
+
+// a,bのうち最大のものをaに入れる(aがbに置き換わるときはtrueを返す)
+template <typename T>
+inline bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+// a,bのうち最小のものをaに入れる(aがbに置き換わるときはtrueを返す)
+template <typename T>
+inline bool chmin(T &a, const T &b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+// 素数判定
+bool is_prime(long long n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 無限大の値
+const long long INF = 1LL << 60;
+
+int main() {
+    string S;cin >> S;
+    double ans = 0.0;
+    vl sumT(S.size(),0);
+    rep(i,0,S.size()){
+        if(i>0){
+            sumT[i] = sumT[i-1];
+        }
+        if(S[i] == 't'){
+            sumT[i]++;
+        }
+    }
+
+    rep(i,0,S.size()){
+        if(S[i] != 't') continue;
+        rep(j,i+2,S.size()){
+            if(S[j] != 't') continue;
+            chmax(ans, ((double)(sumT[j]-sumT[i]-1))/((double)(j-i-1)));
+        }
+    }
+    cout << std::fixed << std::setprecision(15) << ans << "\n";
+}
