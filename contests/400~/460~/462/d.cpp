97772576722b4c9c3185ecd50e21609b93e26ee1
gurukun41 <kouga1582@gmail.com>
2026-07-14 18:26:36 +0900

    462

---
+++ contests/400~/460~/462/d.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using mint = atcoder::modint998244353;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vvvl = vector<vvl>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vvvb = vector<vvb>;
+using vs = vector<string>;
+using vvs = vector<vs>;
+using pl = pair<ll, ll>;
+using vpl = vector<pl>;
+#define rep(i, a, b) for (ll i = (a); i < (ll)(b); i++)
+#define all(v) v.begin(), v.end()
+
+template <typename T>
+inline bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+inline bool chmin(T &a, const T &b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+void yn(bool a) {
+    if (a)
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+}
+
+template <typename T>
+vector<T> read_vector(int n) {
+    vector<T> a(n);
+    rep(i, 0, n) cin >> a[i];
+    return a;
+}
+
+template <typename T>
+vector<vector<T>> read_matrix(int h, int w) {
+    vector<vector<T>> a(h, vector<T>(w));
+    rep(i, 0, h) rep(j, 0, w) cin >> a[i][j];
+    return a;
+}
+
+template <typename T>
+void print_vector(const vector<T> &v, string sep = " ", string end = "\n") {
+    rep(i, 0, v.size()) {
+        if (i) cout << sep;
+        cout << v[i];
+    }
+    cout << end;
+}
+
+template <typename T>
+void print_lines(const vector<T> &v) {
+    for (const T &x : v) cout << x << "\n";
+}
+
+bool inside(int x, int y, int h, int w) {
+    return 0 <= x && x < h && 0 <= y && y < w;
+}
+
+using S = ll;
+using F = ll;
+S op(S a, S b) { return min(a, b); }
+S e() { return (1LL << 60); }
+S mapping(F f, S x) { return x + f; }
+F composition(F f, F g) { return f + g; }
+F id() { return 0; }
+
+int main(){
+    ll N,D;cin >> N >>D;
+    vl a(1e6+2,0);
+    rep(i,0,N){
+        ll Si,T;cin >> Si >> T;
+        Si--;T--;
+        if(T-Si<D)continue;
+        a[Si]++;a[T+1]--;
+    }
+    rep(i,1,N){
+        a[i]+=a[i-1];
+    }
+    atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg(a);
+}
